4e04ad5f2b99584b632972cf7097791a8641ffb1
junddao <y2hunter@gmail.com>
2022-11-01 10:31:11 +0900

    pin design change

--- lib/global/bloc/map/get_pins/get_pins_cubit.dart
+++ lib/global/bloc/map/get_pins/get_pins_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:base_project/global/model/common/api_response.dart';
 import 'package:base_project/global/model/pin/model_request_get_pin.dart';
 import 'package:base_project/global/model/pin/model_response_get_pin.dart';
@@ -96,17 +98,63 @@ class GetPinsCubit extends Cubit<GetPinsState> {
 
     textPainter.layout();
 
-    // Add tag circle
-    canvas.drawRRect(
-        RRect.fromRectAndCorners(
-          Rect.fromLTWH(
-              0.0, 0.0, textPainter.width + 40, textPainter.height + 20),
-          topLeft: radius,
-          topRight: radius,
-          bottomLeft: radius,
-          bottomRight: radius,
-        ),
-        tagPaint);
+    // add shape
+    Size size = Size(textPainter.width + 40, textPainter.height + 40);
+    final bubbleSize = Size(size.width, size.height * 0.8);
+    final tailSize = Size(20, size.height - bubbleSize.height);
+    final fillet = bubbleSize.width * 0.1;
+    final tailStartPoint = Point(size.width * 0.4, bubbleSize.height);
+    //bubble body
+    final bubblePath = Path()
+      ..moveTo(0, fillet)
+      // 왼쪽 위에서 왼쪽 아래 라인
+      ..lineTo(0, bubbleSize.height - fillet)
+      ..quadraticBezierTo(0, bubbleSize.height, fillet, bubbleSize.height)
+      // 왼쪽 아래에서 오른쪽 아래 라인
+      ..lineTo(bubbleSize.width - fillet, bubbleSize.height)
+      ..quadraticBezierTo(bubbleSize.width, bubbleSize.height, bubbleSize.width,
+          bubbleSize.height - fillet)
+      // 오른쪽 아래에서 오른쪽 위 라인
+      ..lineTo(bubbleSize.width, fillet)
+      ..quadraticBezierTo(bubbleSize.width, 0, bubbleSize.width - fillet, 0)
+      // 오른쪽 위에서 왼쪽 아래 라인
+      ..lineTo(fillet, 0)
+      ..quadraticBezierTo(0, 0, 0, fillet);
+    // bubble tail
+    final tailPath = Path()
+      ..moveTo(tailStartPoint.x, tailStartPoint.y)
+      ..cubicTo(
+        tailStartPoint.x + (tailSize.width * 0.4),
+        tailStartPoint.y,
+        tailStartPoint.x + (tailSize.width * 0.6),
+        tailStartPoint.y + (tailSize.height * 0.2),
+        tailStartPoint.x + tailSize.width / 2, // 목적지 x
+        tailStartPoint.y + tailSize.height, // 목적지 y
+      )
+      ..cubicTo(
+        (tailStartPoint.x + tailSize.width / 2) + (tailSize.width * 0.2),
+        tailStartPoint.y + tailSize.height,
+        tailStartPoint.x + tailSize.width,
+        tailStartPoint.y + (tailSize.height * 0.3),
+        tailStartPoint.x + tailSize.width, // 목적지 x
+        tailStartPoint.y, // 목적지 y
+      );
+
+    bubblePath.addPath(tailPath, Offset(0, 0));
+
+    canvas.drawPath(bubblePath, tagPaint);
+
+    // // Add tag circle
+    // canvas.drawRRect(
+    //     RRect.fromRectAndCorners(
+    //       Rect.fromLTWH(
+    //           0.0, 0.0, textPainter.width + 40, textPainter.height + 20),
+    //       topLeft: radius,
+    //       topRight: radius,
+    //       bottomLeft: radius,
+    //       bottomRight: radius,
+    //     ),
+    //     tagPaint);
 
     textPainter.paint(canvas, Offset(20, 10));
 
